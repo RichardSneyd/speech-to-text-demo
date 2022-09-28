59f07709a889b6934188ca0e4202992454167c3d
Richard Sneyd <richardsneyd@hotmail.com>
2022-09-28 11:35:28 +0100

    MVP

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const outputEl = document.getElementById('output');
 const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
 const recognition = SpeechRecognition ? new SpeechRecognition() : false;
 
-const start = ()=> {
-    if(!recognition) {
+const start = () => {
+    if (!recognition) {
         console.log('no SpeechRecognition object');
         document.innerHTML = '<p>Sorry, speech recognition is not supported on your browser. Consider using Chrome.</p>';
         return;
@@ -19,11 +19,19 @@ const stop = () => {
 }
 
 recognition.addEventListener('result', (evt) => {
-    addText(evt.results[evt.results.length-1][0].transcript);
+    addText(evt.results[evt.results.length - 1][0].transcript);
 })
 
 addText = (text) => {
-   outputEl.innerText = outputEl.innerText + text + '. ';
+    text = toSentence(text);
+    outputEl.innerText = outputEl.innerText == '' ? text : outputEl.innerText + ' ' + text;
+}
+
+toSentence = (text) => {
+    text = text.trim().split('');
+    text[0] = text[0].toUpperCase();
+    if (text[text.length - 1] !== '.') text.push('.');
+    return text.join('');
 }
 
 start();
