fc9274b965e39ff13cac641ee6e3a4dbb3b5ce3d
Richard Sneyd <richardsneyd@hotmail.com>
2022-09-28 11:16:06 +0100

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+const outputEl = document.getElementById('output');
+const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+const recognition = SpeechRecognition ? new SpeechRecognition() : false;
+
+const start = ()=> {
+    if(!recognition) {
+        console.log('no SpeechRecognition object');
+        document.innerHTML = '<p>Sorry, speech recognition is not supported on your browser. Consider using Chrome.</p>';
+        return;
+    }
+
+    console.log(recognition);
+    recognition.continuous = true;
+    recognition.start();
+}
+
+const stop = () => {
+    recognition.stop();
+}
+
+recognition.addEventListener('result', (evt) => {
+    addText(evt.results[evt.results.length-1][0].transcript);
+})
+
+addText = (text) => {
+   outputEl.innerText = outputEl.innerText + text + '. ';
+}
+
+start();
